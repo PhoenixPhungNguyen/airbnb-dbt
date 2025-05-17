38ec50a2c15e3c35ca9e57a5b13ffdee83b081e6
PhoenixNguyen <mattaphungnguyen@gmail.com>
2025-05-17 16:02:14 +1000

    remove temporarily s3data large data csv

--- s3data/wget.sh
+++
@@ -1,4 +0,0 @@
-wget dbtlearn.s3.amazonaws.com/reviews.csv
-wget dbtlearn.s3.amazonaws.com/listings.csv
-wget dbtlearn.s3.amazonaws.com/hosts.csv
-wget dbtlearn.s3.amazonaws.com/seed_full_moon_dates.csv
